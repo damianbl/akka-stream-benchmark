6243e88fa964196e1e70a9692b8bd5da12d4c16a
damian <damian@withlocals.com>
2017-11-11 13:31:31 +0100

    benchmarking

--- src/main/scala/com/dblazejewski/akka/stream/AkkaBenchmark.scala
+++ src/main/scala/com/dblazejewski/akka/stream/AkkaBenchmark.scala
@@ -1,29 +1,75 @@
 package com.dblazejewski.akka.stream
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
 import com.typesafe.scalalogging.Logger
 import org.scalameter._
 
-case class StreamWork(from: Int, to: Int)
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class StreamActor extends Actor {
-  private val logger = Logger("StreamActor")
+object Config {
+  val NrOfCores = 8
+}
+
+case class StreamWork(from: Long, to: Long)
+
+class StreamActorFutureSuccesfull(implicit val materializer: ActorMaterializer) extends Actor {
+  private val logger = Logger(classOf[StreamActorFutureSuccesfull])
 
   override def receive: Receive = {
     case m: StreamWork =>
       val time = measure {
         logger.info(s"Processing stream work [$m]")
+
+        val t0 = System.nanoTime()
+
+        Source(m.from to m.to)
+          .mapAsync(Config.NrOfCores)(_ => Future.successful("test-string"))
+          .runWith(Sink.ignore)
+          .onComplete(_ => logger.info(s"Message processing total time: ${(System.nanoTime() - t0) / 1000000} ms"))
       }
 
-      logger.info(s"Total time: $time")
+      logger.info(s"Blocking time in actor: $time")
+  }
+}
+
+class StreamActorFutureApply(implicit val materializer: ActorMaterializer) extends Actor {
+  private val logger = Logger(classOf[StreamActorFutureApply])
+
+  override def receive: Receive = {
+    case m: StreamWork =>
+      val time = measure {
+        logger.info(s"Processing stream work [$m]")
+
+        val t0 = System.nanoTime()
+
+        Source(m.from to m.to)
+          .mapAsync(Config.NrOfCores)(_ => Future("test-string"))
+          .runWith(Sink.ignore)
+          .onComplete(_ => logger.info(s"Message processing total time: ${(System.nanoTime() - t0) / 1000000} ms"))
+      }
+
+      logger.info(s"Blocking time in actor: $time")
   }
 }
 
 object AkkaBenchmark extends App {
 
-  val system: ActorSystem = ActorSystem("akka")
+  implicit val system: ActorSystem = ActorSystem("akka")
 
-  val streamActor: ActorRef = system.actorOf(Props[StreamActor], "streamWork")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-  streamActor ! StreamWork(1, 1000)
+  val streamActorFutureSuccessful: ActorRef = system.actorOf(
+    Props(new StreamActorFutureSuccesfull()), "streamActorFutureSuccessful"
+  )
+
+  val streamActorFutureApply: ActorRef = system.actorOf(
+    Props(new StreamActorFutureApply()), "streamActorFutureApply"
+  )
+
+  streamActorFutureSuccessful ! StreamWork(1L, 10000000L)
+
+  streamActorFutureApply ! StreamWork(1L, 10000000L)
 }
